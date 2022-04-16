1ad6eaf970de0e47ebfde8edebc7e27a348030f1
Cydnee Owens <cks410@yahoo.com>
2022-04-15 20:09:43 -0700

    Update golf course and lunchbox

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,4 +1,29 @@
 class Snack {
+  constructor(type) {
+    this.type           = type;
+    this.amount         = 100;
+    this.isInLunchBox   = false;
+    this.cuttingItClose = false;
+  }
+
+  deliciousLevel() {
+    return 'extra';
+  }
+
+  getEaten() {
+    this.amount -= 10
+    if (this.amount <= (100*0.2)) {
+      this.cuttingItClose = true;
+    }
+  }
+
+  checkForHealthy() {
+    if (this.type.toLowerCase().includes('fruit')) {
+      return true; 
+    } else {
+      return false;
+    }
+  }
 
 }
 
